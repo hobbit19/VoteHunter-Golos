f383cc4fee7ccd6fcc53a9f7d4e3fbf17ab6592f
Roman <roma@mailroma.com>
2018-03-21 18:37:35 +0300

    fixes

--- frontend/main.php
+++ frontend/main.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>YouSource</title>
+	<title>YouSource - media blockchain platform</title>
 	<base href="/">
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
