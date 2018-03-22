f48019768e6d155b6e64828edb2bfc0f36943ff6
Roman <roma@mailroma.com>
2018-03-22 18:17:42 +0300

    oAuth

--- config/web.php
+++ config/web.php
@@ -20,8 +20,22 @@ $config = [
         '@npm'   => '@vendor/npm-asset',
     ],
     'components' => [
+        'authClientCollection' => [
+            'class' => 'yii\authclient\Collection',
+            'clients' => [
+                'google' => [
+                    'class' => 'yii\authclient\clients\Google',
+                    'clientId' => 'google_client_id',
+                    'clientSecret' => 'google_client_secret',
+                ],
+                'facebook' => [
+                    'class' => 'yii\authclient\clients\Facebook',
+                    'clientId' => 'facebook_client_id',
+                    'clientSecret' => 'facebook_client_secret',
+                ],
+            ],
+        ],
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'SKJba!jWHg^626V2S5V2SdDSDC#ddccc',
             'parsers' => [
                 'application/json' => 'yii\web\JsonParser',
