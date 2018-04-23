579d4fd46bc0845c425d89da62cc841ea9388e4c
Roman <roma@mailroma.com>
2018-04-23 19:31:28 +0300

    covers

--- frontend/app/select-cover-popup/select-cover-popup.component.ts
+++ frontend/app/select-cover-popup/select-cover-popup.component.ts
@@ -7,6 +7,15 @@ import {PopupComponent} from '../popup/popup.component';
   styleUrls: ['./select-cover-popup.component.less']
 })
 export class SelectCoverPopupComponent extends PopupComponent implements OnInit {
+    public covers: any;
+
+    ngOnInit() {
+      super.ngOnInit();
+      this.covers = [];
+      for(let i=1; i<=30; i++) {
+          this.covers.push({url: '/covers/th_400_'+i+'.jpg', num: i});
+      }
+    }
 
 }
 
